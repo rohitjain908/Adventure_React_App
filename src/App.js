c2e62e1b646d608c57fc0d3da420a67f9af903fa
Rohit <rrohitjee2018@gmail.com>
2021-05-03 00:02:37 +0530

    Button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+          <Navbar/>
+          <Switch>
+            <Route path='/' exact/>
+          </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
+
+function Navbar(){
+  // constructor(props){
+  //   super(props);
+
+  //   this.state={
+  //     click:'false'
+  //   }
+
+  // }
+  // // state={click:false}
+
+  // handleClick=()=>{
+  //   this.setState({click:!this.state.click})
+
+  // }
+
+  const [click,setClick] = useState(false);
+  const [button,setButton]= useState(false);
+
+  const handleClick=()=> setClick(!click);
+  const closeMobilemenu = () => setClick(false);
+
+  const showButton = () =>{
+    if(window.innerWidth <= 960){
+      setButton(false);
+    }
+    else{
+      setButton(true);
+    }
+  }
+
+  useEffect(() => {
+    showButton()
+  },[]);
+
+  window.addEventListener('resize',showButton);
+
+
+  
+  
+    return(
+      <React.Fragment>
+        <nav className="navbar">
+          <div className="navbar-container">
+            <Link to="/" classname="navbar-logo" onClick={closeMobilemenu}>
+              TRVL <i className="fab fa-typo3"/>
+            </Link>
+            <div className="menu-icon" onClick={ handleClick }>
+              <i className={click ? "fas fa-times" : "fas fa-bars"}/>
+            </div>
+            <ul className={ click ? "nav-menu active":"nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobilemenu}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/services" className="nav-links" onClick={closeMobilemenu}>
+                  Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/products" className="nav-links" onClick={closeMobilemenu}>
+                  Products
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobilemenu}>
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+            { button && <Button buttonStyle="btn--outline">SIGN UP</Button> }
+          </div>
+        </nav>
+      </React.Fragment>
+    )
+  
+}
+
+export default Navbar;
