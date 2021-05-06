0645c3b3254e41360bf047d21daf1a3c94ffa322
Rohit <rrohitjee2018@gmail.com>
2021-05-06 23:53:09 +0530

    Cards and Buttons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+
+
 
 class App extends Component {
   render() {
@@ -11,6 +16,9 @@ class App extends Component {
           <Navbar/>
           <Switch>
             <Route path='/' exact component={Home}/>
+            <Route path="/products" component={Products}/>
+            <Route path="/services" component={Services}/>
+            <Route path="/sign-up" component={SignUp}/>
           </Switch>
       </BrowserRouter>
     );
