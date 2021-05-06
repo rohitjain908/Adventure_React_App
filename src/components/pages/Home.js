9c2b129fc9145ff89959949c5a258e83c33542eb
Rohit <rrohitjee2018@gmail.com>
2021-05-07 01:20:21 +0530

    Footer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,14 @@ import '../../App.css';
 import '../HeroSection.css'
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
+import Footer from '../Footer'
 
 function Home(){
   return (
     <div>
     <HeroSection/>;
     <Cards/>
+    <Footer/>
 
     </div>
   )
