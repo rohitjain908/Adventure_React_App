1f41d9be6fd6ded0d85348e03d5ffe33f2a120d6
Rohit <rrohitjee2018@gmail.com>
2021-05-07 23:25:10 +0530

    Completed

--- src/components/Carditem.js
+++ src/components/Carditem.js
@@ -8,7 +8,7 @@ function Carditem(props) {
       <li className="cards__item">
         <Link className="cards__item__link" to={props.path}>
           <figure className="cards__item__pic-wrap" data-category={props.label}>
-            <img src={props.src} alt="Travel Image" className="cards__item__img"/>
+            <img src={props.src} alt="Travel Imge" className="cards__item__img"/>
           </figure>
           <div className="cards__item__info">
             <h5 className="cards__item__text">{props.text}</h5>
